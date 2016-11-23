1da2eca53560320ae3e6643a1422546bc86f28dc
Kim Kiogora <kimkiogora@gmail.com>
2016-11-23 16:58:33 +0300

    Create CassandraConnector.py

---
+++ CassandraConnector.py
@@ -0,0 +1,8 @@
+# Author    kim kiogora <kimkiogora@gmail.com>
+# Usage     Cassandra C.R.U.D Functionalities
+# Version   1.0
+# Since     11 Nov 2015
+
+class CassandraConnector:
+    def __init__(self):
+        pass
